c4ff6a183c9e00585c28fe5849c644cfb5867668
MithicSpirit <rpc01234@gmail.com>
2020-05-28 09:43:30 -0400

    fixes cd in latex install

--- scripts/latex.bash
+++ scripts/latex.bash
@@ -5,8 +5,14 @@ cd ~/.install-temp
 curl -L http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz > texlive.tar.gz
 tar -xzf texlive.tar.gz
 
-cd texlive
+for DIRECTORY in $(ls | grep install-tl); do
+cd $DIRECTORY
+done
+
 ./install-tl --profile ../files/texlive.profile
 
 tlmgr init-usertree
 tlmgr repository set http://mirror.utexas.edu/ctan/systems/texlive/tlnet
+
+cd ~/.install-temp
+rm texlive
