1f11f0c7e83a42e2b5ecb67f3820120673e7a00e
MithicSpirit <rpc01234@gmail.com>
2020-05-27 21:29:02 -0400

    discord added to misc.bash

--- scripts/misc.bash
+++ scripts/misc.bash
@@ -3,3 +3,6 @@
 cd "~/.install-temp"
 
 sudo apt install tlp powertop xclip
+
+curl -L 'https://canary.discord.com/api/download/canary?platform=linux&format=deb' > discord.deb
+sudo apt install ./discord.deb
