7b3b7da54146d5489f8cff363753e5c505b26ac3
MithicSpirit <rpc01234@gmail.com>
2020-05-30 10:27:05 -0400

    neofetch

--- scripts/misc.bash
+++ scripts/misc.bash
@@ -2,7 +2,7 @@
 
 cd ~/.install-temp
 
-sudo -v apt install tlp powertop xclip gimp krita vlc ffmpeg -y
+sudo -v apt install tlp powertop xclip gimp krita vlc ffmpeg neofetch -y
 sudo -v snap install --beta authy
 
 curl -L 'https://canary.discord.com/api/download/canary?platform=linux&format=deb' > discord.deb
